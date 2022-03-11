99543e843d15eb0a1d5d759e1a5e535aa7431a8e
svgopi <gopinagu2002@gmail.com>
2022-03-11 22:06:07 +0530

    update

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The smallest unit of matter that can exist independently is",
+    answer: "ion",
+    options: [
+      "atom",
+      "molecule",
+      "ion",
+      "radical"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Inside an atom there are",
+    answer: "all of these",
+    options: [
+      "electrons",
+      "protons",
+      "neutrons",
+      "all of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The charge on an electron is ____ and the charge on a proton is ____",
+    answer: "both",
+    options: [
+      "negative",
+      "positive",
+      "both",
+      "none of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The SI unit of length, metre, is denoted by ____and the SI unit of electric current, ampere, is denoted by ____",
+    answer: "both",
+    options: [
+      "m",
+      "A",
+      "both",
+      "none"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A hundredth of a metre is a ______",
+    answer: "centimetre",
+    options: [
+      "centimetre",
+      "multimetre",
+      "both",
+      "none"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Mass and energy are interconvertible",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Cannot be determined",
+      "none"
+    ]
+  }
+];
